f03c29dc793e0f66298d79543681d81624f46635
mcres <marticres@gmail.com>
2019-04-14 13:23:24 +0200

    Update collect_data.py

--- examples/collect_data.py
+++ examples/collect_data.py
@@ -7,15 +7,12 @@ import time
 if __name__ == "__main__":
 
     logging.basicConfig(level=logging.DEBUG)
-    robot = urx.Robot("192.168.1.50", use_rt=False)
+    robot = urx.Robot("192.168.1.100", use_rt=False)
     logging.info("Robot object is available as robot")
-    logging.info("Checking out if robot recieves orders...")
-    robot.set_digital_out(7, True)
+
     fieldnames = {"JointData":["T_motor3", "T_micro1"], "RobotModeData":["isProgramPaused"],
         "ForceModeData":["x","y"]}
-    filepath="data"
+    filepath="robot_data.csv"
     robot.set_data_saved(rate=1,fieldnames=fieldnames,file_path=filepath)
 
-    # select data to be saved
-    #r.set_data_saved(True,1,fieldnames,'/home/marti/Desktop/UR_1.csv')
     time.sleep(10)
